0e26bf6587239a3488e6edf6efe477651ec439b1
osha890 <oshavshav@gmail.com>
2024-05-30 17:03:49 +0300

    asgwqt

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,3 +6,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['username'].help_text = None
+        # self.fields['email'].help_text = None
+        # self.fields['password1'].help_text = None
+        # self.fields['password2'].help_text = None
