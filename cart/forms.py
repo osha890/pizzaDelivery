c49d4138f8fb23a0fedeb1378fe94e682d647ccf
osha890 <oshavshav@gmail.com>
2024-07-04 18:26:55 +0300

    polwkseg

--- cart/forms.py
+++ cart/forms.py
@@ -17,7 +17,7 @@ class AddToCartForm(forms.Form):
         choices=get_sizes(),
         widget=forms.RadioSelect
     )
-    quantity = forms.IntegerField(min_value=1, initial=1)
+    # quantity = forms.IntegerField(min_value=1, initial=1)
     pizza_id = forms.IntegerField(widget=forms.HiddenInput())
 
 
--- pizzaDelivery/views.py
+++ pizzaDelivery/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.utils import timezone
-from pizza.models import Pizza, Size
+from pizza.models import Pizza, Size, Category
 from cart.forms import AddToCartForm
 
 from cart.models import Cart, CartItem
@@ -12,17 +12,17 @@ def pizza_list_view(request):
         if request.user.is_authenticated:
             pizza_id = int(request.POST.get('pizza_id'))
             size_id = int(request.POST.get('size'))
-            quantity = int(request.POST.get('quantity'))
+            # quantity = int(request.POST.get('quantity'))
             cart, created = Cart.objects.get_or_create(user=request.user, defaults={'created_at': timezone.now()})
 
             cart_item, created = CartItem.objects.get_or_create(cart=cart, pizza_id=pizza_id, size=Size.objects.get(id=size_id), defaults={
-                'quantity': quantity,
+                'quantity': 1,
                 'first_added_at': timezone.now(),
                 'last_edited_at': timezone.now()
             })
 
             if not created:
-                cart_item.quantity += quantity
+                cart_item.quantity += 1
                 cart_item.last_edited_at = timezone.now()
                 cart_item.save()
 
@@ -32,11 +32,11 @@ def pizza_list_view(request):
             # return redirect('login')
             return JsonResponse({'status': 'error', 'message': 'User not authenticated'})
     elif request.method == 'GET':
-        pizzas = Pizza.objects.all()
+        cats = Category.objects.all()
         form = AddToCartForm()
         context = {
             'title': 'Home page',
-            'pizzas': pizzas,
+            'cats': cats,
             'form': form,
         }
         return render(request, 'pizza_list.html', context)
